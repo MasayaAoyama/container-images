e3ce28bf76407d0732551fdcb43a1dbe9bad713d
MasayaAoyama <amsy810@gmail.com>
2020-07-28 04:04:39 +0900

    add FILE_PREFIX

--- file-writer/entrypoint.sh
+++ file-writer/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 PART_SIZE=$(( ${SIZE_MB} * 1024 / ${NUM_FILES} ))
+FILE_PREFIX=${FILE_PREFIX:-$HOSTNAME}
 
 COUNT=0
 
@@ -11,7 +12,7 @@ while true; do
   fi
 
   for i in `seq 1 ${NUM_FILES}`; do
-    dd if=/dev/urandom of=${TARGET_DIR}/${HOSTNAME}-${i}.txt bs=1024 count=${PART_SIZE}
+    dd if=/dev/urandom of=${TARGET_DIR}/${FILE_PREFIX}-${i}.txt bs=1024 count=${PART_SIZE}
   done
 
   echo "$COUNT times"
