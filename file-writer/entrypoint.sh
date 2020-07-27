b7ce49f52124ade89a3af2a46d0a2db2ea9c3a0e
MasayaAoyama <amsy810@gmail.com>
2020-07-28 03:30:02 +0900

    add filewriter container

---
+++ file-writer/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+PART_SIZE=$(( $SIZE_MB * 1024 / $NUM_FILES ))
+
+while true; do
+  for i in `seq 1 $NUM_FILES`; do
+    dd if=/dev/urandom of=$TARGET_DIR/$i.txt bs=1024 count=$PART_SIZE
+  done
+done
+
